d32956480554af872d42086384f95a7579a68c71
DEV-NG <nwachukujosh2000@gmail.com>
2024-01-04 17:14:03 +0100

    new commit

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
+# blockchain/urls.py
+
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
-from blockchain import views
-from blockchain.views import *
+from django.urls import path, re_path as url
+from blockchain.views import get_chain, mine_block, is_valid
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('^get_chain$', views.get_chain, name="get_chain"),
-    url('^mine_block$', views.mine_block, name="mine_block"),
-    url('^is_valid$', views.is_valid, name="is_valid"),
+    url('^get_chain$', get_chain, name="get_chain"),
+    url('^mine_block$', mine_block, name="mine_block"),
+    url('^is_valid$', is_valid, name="is_valid"),
 ]
