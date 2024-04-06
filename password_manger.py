734d7c4828870967230ac4cc35a99f9d517c37f8
DEV-NG <nwachukujosh2000@gmail.com>
2024-04-06 19:29:02 +0100

    main

--- password_manger.py
+++ password_manger.py
@@ -1,6 +1,6 @@
 pwd = input("what is the master password?")
 while True:
-    mode = input("would you like to add a new password or view existing")
+    mode = input("would you like to add a new password or view existing ones (view, add)? ")
     if mode == "view":
         pass
     elif mode == "add":
